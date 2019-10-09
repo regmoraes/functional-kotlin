8fd65308fffb9ff7f5848ff390d055bc830882f7
Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>
2019-10-09 00:50:45 -0300

    Add Currying concepts and techniques
    Signed-off-by: Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>

---
+++ src/03_currying.kts
@@ -0,0 +1,53 @@
+/*
+Currying is the technique of translating the evaluation of a function that takes multiple arguments into evaluating
+a sequence of functions, each with a single argument.
+*/
+
+// Two inputs, x and y, one output
+val multiply = { x: Int, y: Int -> x * y }
+println(multiply(3, 5))
+
+/*
+    We want to transform { x: Int, y: Int -> x * y } into two functions that takes one parameter each and chain them
+    to produce the final output. The result function calling will be like this:
+
+    multiply(x)(y)
+
+    This means that multiply(x) would return the function { x, y -> x * y } with set. For example, if we call:
+
+    multiply(3)
+
+    it will return
+
+    { 3, y -> 3 * y }
+
+    Then, we can take the function { 3, y -> 3 * y } and call it passing the y argument:
+
+    { 3, 5 -> 3 * 5 }
+
+    Thus, executing the two functions in sequence
+
+ */
+
+// Currying with method call
+fun curryingMultiply(x: Int): (Int) -> Int {
+    return { y: Int -> x * y }
+}
+
+val returnedFunction = curryingMultiply(3)  // Will return { y -> 3 * y }
+println(
+    returnedFunction(5) // Will execute { 5 -> 3 * 5 } == 15
+)
+
+// Currying with function parameter
+
+/*
+    In the below, curryingMultiply will first return the function { x: Int -> AnotherFunction }.
+    The AnotherFunction that is returned from the first is { y: Int -> x * y }, with the x parameter initialized.
+ */
+val curryingMultiply = { x: Int -> { y: Int -> x * y } }
+
+val partialFunction = curryingMultiply(3) // { y -> 3 * y }
+println(
+    partialFunction(5) // { 5 -> 3 * 5 } == 3 * 5
+)
